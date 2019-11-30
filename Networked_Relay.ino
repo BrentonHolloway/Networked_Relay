ceaeccf350f95fe901114d2ec81ddda2a415e188
BrentonHolloway <brenton.holloway@gmail.com>
2019-11-30 19:13:04 +1100

    new version login

--- Networked_Relay.ino
+++ Networked_Relay.ino
@@ -6,43 +6,7 @@
 #include <SD.h>
 #include <Adafruit_MCP23017.h>
 #include <LiquidCrystal.h>
-
-//buffer size for processing HTML commands
-#define MAXBUFF 40
-
-//Ethernet Server Port
-#define PORT 80
-
-//pins
-#define DS18B20 49 //DS18B20 Pin
-#define MICROSD 4 //Micro SD Card Pin
-
-#define RELAY1 0			//Relay Numbers
-#define RELAY2 1
-#define RELAY3 2
-#define RELAY4 3
-#define RELAY5 4
-#define RELAY6 5
-#define RELAY7 6
-#define RELAY8 7
-
-#define LCDBL 36			//LCD Backling Pin
-#define LEDR 42				//RGB Status LED pins
-#define LEDG 44
-#define LEDB 45
-
-//EEPROM ADDRESSES
-#define CRC32ADDRESS 0
-#define USERADDRESS 4
-#define NETWORKADDRESS 45
-
-//LCD Pins
-#define RS 33
-#define ENABLE 30
-#define D0 37
-#define D1 34
-#define D2 35
-#define D3 32
+#include "Definitions.h"
 
 String readString;
 byte i;
